7ddd180f5cb7c3b606b8ebfa2c660b4b32a8db6b
Thiago Garcia <tgalves18@gmail.com>
2018-03-01 00:16:46 -0300

    Implemented the NSFetchedResultsController variables

--- WorldCup/ViewController.swift
+++ WorldCup/ViewController.swift
@@ -21,12 +21,14 @@
  */
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
   // MARK: - Properties
   fileprivate let teamCellIdentifier = "teamCellReuseIdentifier"
   var coreDataStack: CoreDataStack!
+  var fetchedResultsController: NSFetchedResultsController<Team>!
 
   // MARK: - IBOutlets
   @IBOutlet weak var tableView: UITableView!
@@ -35,6 +37,19 @@ class ViewController: UIViewController {
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let fetchRequest: NSFetchRequest<Team> = Team.fetchRequest()
+    
+    let sort = NSSortDescriptor(key: #keyPath(Team.teamName), ascending: true)
+    fetchRequest.sortDescriptors = [sort]
+    
+    fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: coreDataStack.managedContext, sectionNameKeyPath: nil, cacheName: nil)
+    
+    do {
+      try fetchedResultsController.performFetch()
+    } catch let error as NSError {
+      print("Fetching error: \(error), \(error.userInfo)")
+    }
   }
 }
 
@@ -47,9 +62,10 @@ extension ViewController {
       return
     }
 
-    cell.flagImageView.backgroundColor = UIColor.blue
-    cell.teamLabel.text = "Team Name"
-    cell.scoreLabel.text = "Wins: 0"
+    let team = fetchedResultsController.object(at: indexPath)
+    cell.flagImageView.image = UIImage(named: team.imageName!)
+    cell.teamLabel.text = team.teamName
+    cell.scoreLabel.text = "Wins: \(team.wins)"
   }
 }
 
@@ -57,11 +73,17 @@ extension ViewController {
 extension ViewController: UITableViewDataSource {
 
   func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
+    guard let sections = fetchedResultsController.sections else {
+      return 0
+    }
+    return sections.count
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 20
+    guard let sectionInfo = fetchedResultsController.sections?[section] else {
+      return 0
+    }
+    return sectionInfo.numberOfObjects
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
